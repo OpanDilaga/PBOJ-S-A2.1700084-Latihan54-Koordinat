227632b9bde02616e0df9c66f07f8b699261a68c
OpanDilaga <51019063+OpanDilaga@users.noreply.github.com>
2019-05-29 13:28:31 +0700

    Java
    Upload Java

---
+++ PBOJ&S-A2.1700084-Latihan54/src/latihan/pkg54/Latihan54.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan.pkg54;
+
+/**
+ *
+ * @author acer
+ * Nama     : Opan Dilaga
+ * NIM      : A2.1700084
+ * Kelas    : TI IV E J&S
+ */
+public class Latihan54 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+      WarnaKoordinat Latihan54 = new WarnaKoordinat("jingga",10,4);
+      System.out.println("Warna Koordinat :"+Latihan54.getNamaWarna());
+      System.out.println("Koordinat sumbu x :"+Latihan54.getX()+", y :"+Latihan54.getY());
+      
+    }
+      
+      
+      
+      
+    
+}
